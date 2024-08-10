4bb13d2e419ba64eb58a7490bc37fa2996ce4c0c
Aaron Abraham <aaronabraham1509@gmail.com>
2024-08-10 10:32:46 +0530

    Post Input Cache Mutation added

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -8,9 +8,13 @@ import { useSession } from "@/app/(main)/SessionProvider"
 import UserAvatar from "@/components/UserAvatar"
 import { Button } from "@/components/ui/button"
 import "./styles.css"
+import { useSubmitPostMutation } from "./mutations"
+import LoadingButton from "@/components/LoadingButton"
 
 export default function PostEditor() {
     const { user } = useSession(); 
+
+    const mutation = useSubmitPostMutation();
 
     const editor = useEditor({
         extensions: [
@@ -28,8 +32,12 @@ export default function PostEditor() {
         blockSeparator: "\n",
     }) || "";
 
-    async function onSubmit() {
-        await submitPost(input)
+    function onSubmit() {
+        mutation.mutate(input, {
+            onSuccess: () => {
+                editor?.commands.clearContent();
+            }
+        })
         editor?.commands.clearContent();
     }
 
@@ -42,13 +50,14 @@ export default function PostEditor() {
             />
         </div>
         <div className="flex justify-end">
-            <Button
+            <LoadingButton
+            loading = {mutation.isPending}
             onClick={onSubmit}
             disabled = { !input.trim() }
             className="min-w-20"
             >
                 Post
-            </Button>
+            </LoadingButton>
         </div>
     </div>
 }
