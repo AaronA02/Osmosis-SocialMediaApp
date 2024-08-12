ce0c614484a65c648cee1061fd98a2ff562bd5f9
Aaron Abraham <aaronabraham1509@gmail.com>
2024-08-12 09:53:52 +0530

    User Profile created

---
+++ src/app/(main)/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="my-12 w-full space-y-3 text-center">
+      <h1 className="text-3xl font-bold">Not Found</h1>
+      <p>The page you&apos;re looking for does not exist</p>
+    </main>
+  );
+}
--- src/components/posts/editor/mutations.ts
+++ src/components/posts/editor/mutations.ts
@@ -1,59 +1,76 @@
 import { useToast } from "@/components/ui/use-toast";
-import { InfiniteData, QueryFilters, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  InfiniteData,
+  QueryFilters,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { submitPost } from "./actions";
 import { PostsPage } from "@/lib/types";
+import { useSession } from "@/app/(main)/SessionProvider";
 
 export function useSubmitPostMutation() {
-    const { toast } = useToast();
+  const { toast } = useToast();
 
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const mutation = useMutation({
-        mutationFn: submitPost,
-        onSuccess: async (newPost) => {
-            const queryFilter: QueryFilters = { queryKey: ["post-feed", "for-you"]};
+  const { user } = useSession();
 
-            await queryClient.cancelQueries(queryFilter);
+  const mutation = useMutation({
+    mutationFn: submitPost,
+    onSuccess: async (newPost) => {
+      const queryFilter = {
+        queryKey: ["post-feed"],
+        predicate(query) {
+          return (
+            query.queryKey.includes("for-you") ||
+            (query.queryKey.includes("user-posts") &&
+              query.queryKey.includes(user.id))
+          );
+        },
+      } satisfies QueryFilters;
 
-            queryClient.setQueriesData<InfiniteData<PostsPage, string | null>> (
-                queryFilter,
-                (oldData) => {
-                    const firstPage = oldData?.pages[0];
+      await queryClient.cancelQueries(queryFilter);
 
-                    if (firstPage) {
-                        return {
-                            pageParams: oldData.pageParams,
-                            pages: [
-                                {
-                                    posts: [newPost, ...firstPage.posts],
-                                    nextCursor: firstPage.nextCursor,
-                                },
-                                ...oldData.pages.slice(1),
-                            ]
-                        }
-                    }
-                }
-            );
+      queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>(
+        queryFilter,
+        (oldData) => {
+          const firstPage = oldData?.pages[0];
 
-            queryClient.invalidateQueries({
-                queryKey: queryFilter.queryKey,
-                predicate(query) {
-                    return !query.state.data;
+          if (firstPage) {
+            return {
+              pageParams: oldData.pageParams,
+              pages: [
+                {
+                  posts: [newPost, ...firstPage.posts],
+                  nextCursor: firstPage.nextCursor,
                 },
-            });
-
-            toast({
-                description: "Post Created",
-            })
+                ...oldData.pages.slice(1),
+              ],
+            };
+          }
         },
-        onError(error) {
-            console.error(error);
-            toast({
-                variant: "destructive",
-                description: "Failed to post. Please try again."
-,            });
-        },
-    });
+      );
 
-    return mutation;
+      queryClient.invalidateQueries({
+        queryKey: queryFilter.queryKey,
+        predicate(query) {
+          return queryFilter.predicate(query) && !query.state.data;
+        },
+      });
+
+      toast({
+        description: "Post Created",
+      });
+    },
+    onError(error) {
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Failed to post. Please try again.",
+      });
+    },
+  });
+
+  return mutation;
 }
