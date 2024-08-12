a690a57cd53d4a80dc39b22ee797459e1bc14a75
Aaron Abraham <aaronabraham1509@gmail.com>
2024-08-12 08:22:43 +0530

    For-you, Following tabs done

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,20 +4,33 @@ import TrendsSidebar from "@/components/TrendsSidebar";
 import prisma from "@/lib/prisma";
 import ForYouFeed from "./ForYouFeed";
 import { getPostDataInclude } from "@/lib/types";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 
 export default async function Home() {
   const posts = await prisma.post.findMany({
-    include: getPostDataInclude(user.id),
-    orderBy: { createdAt: "desc"}
-  })
+include: getPostDataInclude(user?.id),
+    orderBy: { createdAt: "desc" },
+  });
 
-  return(
-    <main className="w-full min-w-0 flex gap-5">
+  return (
+    <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
-        <PostEditor/>
-        <ForYouFeed/>
+        <PostEditor />
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
-      <TrendsSidebar/>
+      <TrendsSidebar />
     </main>
   );
 }
